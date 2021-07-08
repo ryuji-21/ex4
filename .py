a840c63c819d91cd1724b1db235186577d2675d8
ryuji-21 <86811846+ryuji-21@users.noreply.github.com>
2021-07-08 17:33:00 +0900

    Update and rename .gitignore to .py

---
+++ .py
@@ -0,0 +1,9 @@
+import random
+
+print("Rolling the dice...")
+Die1 = random.randint(1,6)
+Die2 = random.randint(1,6)
+A = Die1 + Die2
+print("Die 1:",Die1)
+print("Die 2:",Die2)
+print("Total value:",A)
